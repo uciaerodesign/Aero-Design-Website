50890f8a3871321a742752cdfbd22470c1bc6db0
Zhiyuan Rong <zhiyuar2@uci.edu>
2026-09-18 10:11:30 -0700

    Add Vercel Speed Insights

--- areofrontend/src/app/layout.js
+++ areofrontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata = {
   title: "UCI Aero Design",
   description: "Official website for UCI Aero Design",
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
