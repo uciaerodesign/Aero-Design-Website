48b9e37abed7ea6e12757877b6cd2d751006d0cf
Zhiyuan Rong <zhiyuar2@uci.edu>
2026-09-18 10:09:45 -0700

    Add Vercel Analytics

--- areofrontend/src/app/layout.js
+++ areofrontend/src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next";
 export const metadata = {
   title: "UCI Aero Design",
   description: "Official website for UCI Aero Design",
@@ -13,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
